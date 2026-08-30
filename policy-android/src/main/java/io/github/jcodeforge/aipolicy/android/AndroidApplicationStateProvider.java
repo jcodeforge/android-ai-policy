c46d3a04db994948d2202c07ce92f8049049284c
al.scholz <a.scholz@fuhrpark-software.de>
2026-08-30 10:10:31 +0200

    Add DefaultAndroidApplicationStateProvider

---
+++ policy-android/src/main/java/io/github/jcodeforge/aipolicy/android/AndroidApplicationStateProvider.java
@@ -0,0 +1,8 @@
+package io.github.jcodeforge.aipolicy.android;
+
+import io.github.jcodeforge.aipolicy.ApplicationState;
+
+public interface AndroidApplicationStateProvider {
+
+    ApplicationState getApplicationState();
+}
---
+++ policy-android/src/main/java/io/github/jcodeforge/aipolicy/android/DefaultAndroidApplicationStateProvider.java
@@ -0,0 +1,36 @@
+package io.github.jcodeforge.aipolicy.android;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ProcessLifecycleOwner;
+import io.github.jcodeforge.aipolicy.ApplicationState;
+
+/**
+ * Default implementation that tracks the application's foreground/background
+ * state using ProcessLifecycleOwner.
+ */
+public final class DefaultAndroidApplicationStateProvider implements AndroidApplicationStateProvider,
+        DefaultLifecycleObserver {
+
+    private volatile ApplicationState state = ApplicationState.UNKNOWN;
+
+    public DefaultAndroidApplicationStateProvider() {
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(this);
+    }
+
+    @Override
+    public ApplicationState getApplicationState() {
+        return state;
+    }
+
+    @Override
+    public void onStart(@NonNull LifecycleOwner owner) {
+        state = ApplicationState.FOREGROUND;
+    }
+
+    @Override
+    public void onStop(@NonNull LifecycleOwner owner) {
+        state = ApplicationState.BACKGROUND;
+    }
+}
